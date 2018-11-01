8fc0e5df1bbdd1eab89950c2af7cb7021178b161
Hanninenjp <jphanninen@gmail.com>
2018-11-01 12:47:08 +0200

    Getting started with Angular demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KLI1317-contacts-app';
+  name: string;
+  greeting: string;
+  greetings: string[];
+
+  constructor() {
+    this.greeting = 'Hello, World';
+    this.name = 'World';
+    this.greetings = [];
+  }
+
+  onSubmitGreeting() {
+    const submittedGreeting = 'Hello, ' + this.name + '!';
+    this.greetings.push(submittedGreeting);
+    console.log(this.greetings);
+  }
 }
