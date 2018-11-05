3e887c746a705ff28101c5ff5e57be3bfb9cde3b
Hanninenjp <jphanninen@gmail.com>
2018-11-05 19:50:55 +0200

    Added ToolbarComponent and implemented basic application toolbar with a menu button and application title.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import { ContactListComponent } from './contact/contact-list/contact-list.compon
 import { ContactListItemComponent } from './contact/contact-list-item/contact-list-item.component';
 import {ContactService} from './contact/services/contact.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './ui/toolbar/toolbar/toolbar.component';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    ContactListItemComponent
+    ContactListItemComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [ContactService],
   bootstrap: [AppComponent]
