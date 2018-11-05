3da3cc558ce68874536f710190d33018d467b6ea
Hanninenjp <jphanninen@gmail.com>
2018-11-05 19:09:11 +0200

    Created initial component and service architecture for the contacts application. Added Contact model class, ContactList and ContactListItem components, and ContactService. Implemented basic list view for contacts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'KLI1317-contacts-app';
-  name: string;
-  greeting: string;
-  greetings: string[];
 
   constructor() {
-    this.greeting = 'Hello, World';
-    this.name = 'World';
-    this.greetings = [];
-  }
-
-  onSubmitGreeting() {
-    const submittedGreeting = 'Hello, ' + this.name + '!';
-    this.greetings.push(submittedGreeting);
-    console.log(this.greetings);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list-item/contact-list-item.component';
+import {ContactService} from './contact/services/contact.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
